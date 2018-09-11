678be9f09dfb452c2838b2f8e4e058d445e24278
Meenakshi-Anand <abhi@codeanand.com>
2018-09-11 13:01:25 -0700

    switch route set up completed

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
+import {BrowserRouter,Redirect,Switch,Route} from 'react-router-dom';
 import LandingPage from './information/landing_page';
 import LoopingBackground from './information/loop_video_backgrounds';
 const App = () => (
-  <div>
+
+    <BrowserRouter>
     <Switch>
     <Route exact path="/" component={LandingPage} />
     <Route exact path="/loopingbackground" component={LoopingBackground} />
     <Redirect to="/" />
     </Switch>
-  </div>
+    </BrowserRouter>
+
 );
 
 export default App;
