f8edd3665ebe02ef2f3dfefe7d100c505776f978
Meenakshi-Anand <abhi@codeanand.com>
2018-09-11 12:35:38 -0700

    changed class name in landing page

--- frontend/components/information/landing_page.jsx
+++ frontend/components/information/landing_page.jsx
@@ -1,17 +1,17 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router';
-class Example extends Component {
+class LandingPage extends Component {
     render () {
         return (
             <div>
               <h1> Components 101 </h1>
               <h4> Build fun components with us !</h4>
               <ol>
-              <li><Link to={"/loopingbackground"}><Example /></Link></li>
+              <li><Link to={"/loopingbackground"}>Looping Background</Link></li>
               </ol>
             </div>
         );
     }
 }
 
-export default Example;
+export default LandingPage;
